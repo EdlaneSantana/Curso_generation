50406263ab78db2c9792372e13067e2cdd0b0d65
EdlaneSantana <edlane.santana@mercadolivre.com>
2023-01-10 11:49:02 -0300

    Update TodasAtividades.java

--- TodasAtividades.java
+++ TodasAtividades.java
@@ -69,6 +69,26 @@ em variáveis do tipo float e exiba na tela a média final do participante. Veja
         salarioLiquido = salarioBruto + adicionalNoturno + (horasExtras * 5) - descontos;
 
         System.out.printf("O salário líquido é: R$%.2f", salarioLiquido);
+        
+        
+ // Leia quatro valores float (numero1, numero2, numero3 e numero4). A seguir, calcule e mostre a diferença
+// do produto entre o numero1 e o numero2 pelo produto entre o numero3 e o numero4.
+
+        float numero1, numero2, numero3, numero4;
+
+        System.out.println("Digite o primeiro número, separdo por ponto: ");
+        numero1 = sc.nextFloat();
+
+        System.out.println("Digite o segundo número, separdo por ponto: ");
+        numero2 = sc.nextFloat();
+
+        System.out.println("Digite o terceiro número, separdo por ponto: ");
+        numero3= sc.nextFloat();
+
+        System.out.println("Digite o quarto numero, separdo por ponto: ");
+        numero4 = sc.nextFloat();
+
+        System.out.printf("Diferença: %.1f", ((numero1*numero2)-(numero3 * numero4)));
 
         sc.close();
     }
